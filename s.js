b35ce4b3301171128eee1189abdf260ea26a5eb4
sanyazavod <51849970+sanyazavod@users.noreply.github.com>
2026-06-18 01:21:12 +0300

    41234321

--- s.js
+++ s.js
@@ -2,36 +2,21 @@
     'use strict';
 
     function init() {
-        // Убедимся, что мы добавляем объект с обязательными полями
-        Lampa.Settings.push({
-            name: 'my_test_plugin', // Уникальный идентификатор
-            label: 'Моя страница',   // Заголовок, который видит пользователь
-            type: 'button',
-            onClick: function() {
-                Lampa.Activity.push({
-                    url: '',
-                    title: 'Приветствие', // Заголовок страницы
-                    component: 'test_page',
-                    page: 1
-                });
-            }
-        });
+        console.log('Мой плагин инициализирован');
 
-        Lampa.Component.add('test_page', function() {
-            this.create = function() {
-                var div = document.createElement('div');
-                div.style.padding = '50px';
-                div.style.textAlign = 'center';
-                div.innerHTML = '<h1>Привет!</h1>';
-                return div;
-            };
-            this.render = function() { return this.create(); };
+        // Пример: добавить пункт в меню
+        Lampa.Listener.follow('app', function(e) {
+            if (e.type === 'ready') {
+                console.log('Lampa готова!');
+            }
         });
     }
 
-    if (window.lampa) {
+    // Правильная проверка
+    if (window.Lampa && Lampa.Listener) {
         init();
     } else {
-        window.addEventListener('lampa:ready', init);
+        // Правильное событие
+        document.addEventListener('ready', init);
     }
 })();
